14a65bb0f3c1ce2fb1e5c6fd3c331a5cacad5436
unknown <grnx.git@gmail.com>
2025-03-19 17:06:13 +0100

    add color coded approximation lines

--- FunctionIntervalApproximation.py
+++ FunctionIntervalApproximation.py
@@ -1,6 +1,7 @@
 import sympy as sp
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.colors as mcolors
 
 # First time working with sympy, i need all the comments to memorize
 # what my pastes from the docs actually do
@@ -15,7 +16,7 @@ x = sp.symbols('x')
 f = sp.exp(x)
 
 # Number of approximation iterations/degree of polynomial
-num_iterations = 5
+num_iterations = 10
 
 P = 0 # P = polynomial
 
@@ -28,7 +29,7 @@ for i in range(num_iterations):
     P += term # append term to the polynomial
     polynomials.append(P)
 
-
+print(polynomials)
 
 # plots
 x_vals = np.linspace(-2, 2, 400)
@@ -36,17 +37,23 @@ f_vals = [sp.N(f.subs(x, val)) for val in x_vals]
 
 plt.plot(x_vals, f_vals, label='Original function $e^x$', color='black')
 
+# create a color spectrum
+cmap = plt.cm.rainbow
+colors = [cmap(i / (num_iterations - 1)) for i in range(num_iterations)]
+
 for i, poly in enumerate(polynomials):
     poly_vals = [sp.N(poly.subs(x, val)) for val in x_vals]
     if i == num_iterations - 1:
-        plt.plot(x_vals, poly_vals, label=f'Approximation {i+1}', color='blue')
+        plt.plot(x_vals, poly_vals, label=f'Approximation {i+1}', color=colors[i])
     else:
-        plt.plot(x_vals, poly_vals, label=f'Approximation {i+1}', color='blue', alpha=0.3)
+        # not the final approximation -> make plot line opaque
+        plt.plot(x_vals, poly_vals, label=f'Approximation {i+1}', color=colors[i], alpha=0.4)
 
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title(f'Taylor Series Approximation of {sp.latex(f)} at x = {x_value}')
-plt.legend()
+#plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.legend(fancybox=True, framealpha=0.5, fontsize='x-small')
 plt.grid(True)
 plt.show()
 
@@ -58,3 +65,4 @@ print(f"Original function: {sp.sstr(f)}")
 print(f"Original function: {str(f)}")"""
 
 print(f"Final approximation: {polynomials[-1]}")
+print(f"in LaTeX: {sp.latex(polynomials[-1])}")
